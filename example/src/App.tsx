922a215ca6af8379da07f7e6d153e09ba03e17ce
Médédé Raymond KPATCHAA <kraymond70@gmail.com>
2025-01-12 01:15:27 +0100

    fix: upgrade dependencies

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import './App.css';
+import './App.css'
+
 import Form from './Form';
 import yourDetails from './states/yourDetails';
-import { StateMachineProvider, createStore } from 'little-state-machine';
+import { createStore } from 'little-state-machine';
 import { DevTool } from './src';
 
 createStore(
@@ -11,16 +11,16 @@ createStore(
   },
 );
 
-const App: React.FC = () => {
+function App() {
   return (
-    <StateMachineProvider>
+    <>
       <DevTool />
       <div className="App">
         <h1>Little State Machine</h1>
         <Form />
       </div>
-    </StateMachineProvider>
+    </>
   );
-};
+}
 
-export default App;
+export default App
